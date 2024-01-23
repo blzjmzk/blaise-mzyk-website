93c6ac009c178378aeae94c634d4474ae50b76f9
Błażej Mzyk <blazej.mzyk@gmail.com>
2024-01-23 16:02:09 +0100

    Add displaying posts in admin panel

--- app/blaise/page.tsx
+++ app/blaise/page.tsx
@@ -1,7 +1,45 @@
-import React from "react";
+import styles from "./PostsPage.module.css";
+import prisma from "@/prisma/client";
 
-const Posts = () => {
-  return <div>Posts</div>;
+const PostsPage = async () => {
+  const posts = await prisma.post.findMany();
+
+  return (
+    <table className={styles.Table}>
+      <thead>
+        <tr>
+          <th className={styles.TableData}>Id</th>
+          <th className={styles.TableData}>Title</th>
+          <th className={styles.TableData}>Category</th>
+          <th className={styles.TableData}>Publishing Time</th>
+          <th className={styles.TableData}>Updated Time</th>
+          <th className={styles.TableData}>Edit</th>
+          <th className={styles.TableData}>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        {posts.map((post) => (
+          <tr key={post.id}>
+            <td className={styles.TableData}>{post.id}</td>
+            <td className={styles.TableData}>{post.title}</td>
+            <td className={styles.TableData}>{post.category}</td>
+            <td className={styles.TableData}>
+              {post.publishedAt.toDateString()}
+            </td>
+            <td className={styles.TableData}>
+              {post.updatedAt?.toDateString()}
+            </td>
+            <td className={styles.TableData}>
+              <button>Edit</button>
+            </td>
+            <td className={styles.TableData}>
+              <button>Delete</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
-export default Posts;
+export default PostsPage;
