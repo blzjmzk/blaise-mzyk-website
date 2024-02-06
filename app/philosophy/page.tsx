7911ec4e8a9fad44889eddc883c54a185af020aa
Błażej Mzyk <blazej.mzyk@gmail.com>
2024-02-06 16:42:37 +0100

    Add tagline in philosophy section

--- app/philosophy/page.tsx
+++ app/philosophy/page.tsx
@@ -1,4 +1,9 @@
 import prisma from "@/prisma/client";
+import {
+  NumberCircleOne,
+  NumberCircleThree,
+  NumberCircleTwo,
+} from "@phosphor-icons/react/dist/ssr"; //Licence: MIT
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
@@ -21,8 +26,62 @@ const PhilosophyPage = async () => {
   return (
     <>
       <Header>Philosophy</Header>
+      <section className={styles.philosophyTagline}>
+        <div className={styles.philosophyTaglineItem}>
+          <div>
+            <NumberCircleOne
+              size={60}
+              weight="bold"
+              color="var(--color-primary)"
+            />
+          </div>
+          <p>
+            I am currently a <span className={styles.bold}>researcher</span> at
+            the Jagiellonian University in Kraków, Poland, where{" "}
+            <span className={styles.bold}>I manage the project</span>{" "}
+            &quot;Truthmaking. Are Facts Still Really Indispensable?&quot;
+            (Funding: Ministry of Education and Science of the Republic of
+            Poland, Program: Diamentowy Grant IX Edition 2020-2024, Budget:
+            approx. 180K PLN/42K USD, Role: Principal Investigator)
+          </p>
+        </div>
+        <div className={styles.philosophyTaglineItem}>
+          <div>
+            <NumberCircleTwo
+              size={60}
+              weight="bold"
+              color="var(--color-primary)"
+            />
+          </div>
+          <p>
+            I am also a <span className={styles.bold}>doctoral student</span> at
+            the Jagiellonian University Doctoral School in the Humanities. I am
+            working on a <span className={styles.bold}>PhD thesis</span> about
+            the criticism of David Armstrong&apos;s standard theory of
+            truthmaking over the past 20 years.
+          </p>
+        </div>
+        <div className={styles.philosophyTaglineItem}>
+          <div>
+            <NumberCircleThree
+              size={60}
+              weight="bold"
+              color="var(--color-primary)"
+            />
+          </div>
+          <p>
+            My <span className={styles.bold}>area of specialization</span> is
+            contemporary analytic philosophy in the field of metaphysics. My
+            research focuses on the{" "}
+            <span className={styles.bold}>theory of truthmaking</span> (with a
+            particular emphasis on the relation between the substantive and
+            deflationary approaches to truthmaking) and the{" "}
+            <span className={styles.bold}>concept of fact</span>.
+          </p>
+        </div>
+      </section>
       <SectionHeading>My Publications</SectionHeading>
-      <div className={styles.publicationsContainer}>
+      <section className={styles.publicationsContainer}>
         <div className={styles.publicationsTagline}>
           I publish under the name &quot;Błażej Mzyk&quot;, which is the Polish
           form of my first and last name. Clicking on the title of a publication
@@ -86,20 +145,22 @@ const PhilosophyPage = async () => {
             }
           })}
         </ul>
-      </div>
-      <SectionHeading>My Talks</SectionHeading>
-      <ul className={styles.talksList}>
-        {talks.map((talk) => (
-          <li key={talk.id} className={styles.listItem}>
-            {`${talk.title}, `}
-            <span className={styles.italic}>{talk.conference}</span>
-            {`, ${talk.details}, ${talk.year}`}
-            {talk.isOnline && ` [online]`}
-          </li>
-        ))}
-      </ul>
+      </section>
+      <section>
+        <SectionHeading>My Talks</SectionHeading>
+        <ul className={styles.talksList}>
+          {talks.map((talk) => (
+            <li key={talk.id} className={styles.listItem}>
+              {`${talk.title}, `}
+              <span className={styles.italic}>{talk.conference}</span>
+              {`, ${talk.details}, ${talk.year}`}
+              {talk.isOnline && ` [online]`}
+            </li>
+          ))}
+        </ul>
+      </section>
       <SectionHeading>See Also My Profiles at:</SectionHeading>
-      <div className={styles.profilesContainer}>
+      <section className={styles.profilesContainer}>
         <Link
           href={"https://philpeople.org/profiles/blazej-mzyk"}
           target="_blank"
@@ -132,7 +193,7 @@ const PhilosophyPage = async () => {
             }}
           />
         </Link>
-      </div>
+      </section>
       <Link
         href="https://philpeople.org/public_cache/file?content_type=application%2Fpdf&key=4ubyqbhxg36iu2t52wq6rbojac3b"
         className={styles.cvButton}
