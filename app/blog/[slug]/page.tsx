7bef840960b21d87aedb0fd9c77bfb4eaf1cf165
Błażej Mzyk <blazej.mzyk@gmail.com>
2024-05-16 15:33:10 +0200

    Add syntax highlight

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,30 +1,107 @@
+import Button from "@/components/button";
 import Header from "@/components/header";
 import prisma from "@/prisma/client";
-import { notFound } from "next/navigation";
-import { cache, useEffect } from "react";
-import ReactMarkdown from "react-markdown";
-import styles from "./PostPage.module.css";
 import Link from "next/link";
-import Button from "@/components/button";
+import { notFound } from "next/navigation";
+import rangeParser from "parse-numeric-range";
+import { cache } from "react";
+import ReactMarkdown from "react-markdown";
+import {
+  PrismLight as SyntaxHighlighter,
+  SyntaxHighlighterProps,
+} from "react-syntax-highlighter";
+import json from "react-syntax-highlighter/dist/cjs/languages/prism/json";
+import tsx from "react-syntax-highlighter/dist/cjs/languages/prism/tsx";
+import typescript from "react-syntax-highlighter/dist/cjs/languages/prism/typescript";
+import { oneLight } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { Node } from "unist";
+import styles from "./PostPage.module.css";
+
+SyntaxHighlighter.registerLanguage("tsx", tsx);
+SyntaxHighlighter.registerLanguage("typescript", typescript);
+SyntaxHighlighter.registerLanguage("json", json);
 
 interface Props {
   params: { slug: string };
+}
+
+interface CodeProps {
+  node: Node;
+  inline: boolean;
+  className: string;
+}
+
+interface NodeWithData extends Node {
+  data?: {
+    meta?: string;
+  };
 }
 
 const fetchPost = cache((postSlug: string) =>
   prisma.post.findUnique({ where: { slug: postSlug } })
 );
 
+interface HighlighterProps extends SyntaxHighlighterProps {
+  children: string | string[];
+}
+
 const PostPage = async ({ params }: Props) => {
   const post = await fetchPost(params.slug);
 
   if (!post) return notFound();
+  const syntaxTheme = oneLight;
+
+  const MarkdownComponents: object = {
+    code({ node, inline, className, ...props }: CodeProps & HighlighterProps) {
+      const hasMeta = (node as NodeWithData)?.data?.meta;
+
+      const applyHighlights: object = (applyHighlights: number) => {
+        if (hasMeta) {
+          const RE = /{([\d,-]+)}/;
+          const metadata = (node as NodeWithData)?.data?.meta?.replace(
+            /\s/g,
+            ""
+          );
+          const strlineNumbers = RE.test(metadata!)
+            ? RE.exec(metadata!)?.[1] || "0"
+            : "0";
+          const highlightLines = rangeParser(strlineNumbers);
+          const data: string | null = highlightLines.includes(applyHighlights)
+            ? "highlight"
+            : null;
+          return { data };
+        } else {
+          return {};
+        }
+      };
+
+      return (
+        <SyntaxHighlighter
+          style={syntaxTheme}
+          language={className.replace("language-", "")}
+          PreTag="div"
+          className="codeStyle"
+          showLineNumbers={true}
+          wrapLines={!!hasMeta}
+          useInlineStyles={true}
+          lineProps={applyHighlights}
+          {...props}
+        >
+          {Array.isArray(props.children)
+            ? props.children.join("")
+            : props.children}
+        </SyntaxHighlighter>
+      );
+    },
+  };
 
   return (
     <>
       <Header>{post.title}</Header>
       <div className={styles.postContainer}>
-        <ReactMarkdown>{post.content}</ReactMarkdown>
+        <ReactMarkdown components={MarkdownComponents}>
+          {post.content}
+        </ReactMarkdown>
       </div>
       <Link href="/blog">
         <div className={styles.postButton}>
